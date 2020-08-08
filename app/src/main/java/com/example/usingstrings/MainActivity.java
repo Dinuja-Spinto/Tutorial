20a377aa6389e04071195dc669a78b798b9bbd69
Dinuja-Spinto <sri.prabasha@gmail.com>
2020-08-08 19:35:57 +0530

    Initial Commit....!!!!

---
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.usingstrings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txtVtMsg2 = findViewById(R.id.TvMsg2);
+        txtVtMsg2.setText(R.string.Msg2);
+
+        Log.i("Lifecycle", "OnCreate Called..");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i("Lifecycle", "OnStart Called..");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i("Lifecycle", "onResume Called..");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i("Lifecycle", "onPause Called..");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i("Lifecycle", "onStop Called..");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i("Lifecycle", "onDestroy Called..");
+    }
+}
